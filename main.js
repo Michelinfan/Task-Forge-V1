47ce12d2ac6b09d5ed686383ec2d7ea2ce330ad1
Michelinfan <diego.sanz@valencia.salesianas.org>
2026-09-09 20:39:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1180, height: 760, minWidth: 900, minHeight: 600,
+    webPreferences: { contextIsolation: true }
+  });
+  win.loadFile("index.html");
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+});
+app.on("window-all-closed", () => { if (process.platform !== "darwin") app.quit(); });
