14a2dae89783b58dda501f36563289bdb22a11c3
Michelinfan <diego.sanz@valencia.salesianas.org>
2026-09-14 17:40:47 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("taskforge", {
-  showNotification: ({ title, body }) => ipcRenderer.send("show-notification", { title, body }),
-  syncTasks: (tasks) => ipcRenderer.send("sync-tasks", tasks),
-  getStoredTasks: () => ipcRenderer.invoke("get-stored-tasks")
-});
